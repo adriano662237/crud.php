f125993fa05a4cc5e083bc711bd8ab03e2dc76e8
adriano662237 <84392578+adriano662237@users.noreply.github.com>
2022-05-31 11:09:18 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+
+    $servidor = "localhost";
+    $usuario = "id18876700_root";
+    $senha = "<eBl1/NNRYh#\!(a";
+    $banco = "id18876700_db_login";
+    $porta = "3306";
+
+    $conexao = mysqli_connect($servidor, $usuario, $senha, $banco, $porta) or die ('Não foi possível conectar!');
+    
+    echo 'A conexão foi efetuada com sucesso!';
+
+?>
